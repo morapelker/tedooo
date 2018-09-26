b9855ca78e37202ab832467f503aad7efc1e144c
Mor <morapelker@gmail.com>
2018-09-26 21:39:11 +0300

    sd

--- src/api/apiConstants.js
+++ src/api/apiConstants.js
@@ -1,2 +1,2 @@
-// export const URL = 'https://baloofeathers.herokuapp.com/';
-export const URL = 'http://localhost:3030/';
+export const URL = 'https://baloofeathers.herokuapp.com/';
+// export const URL = 'http://localhost:3030/';
