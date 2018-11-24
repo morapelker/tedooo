96ce1253afdf5514771703811c2b906f25ab4745
Mor <morapelker@gmail.com>
2018-11-25 00:20:29 +0200

    sd

--- src/api/apiConstants.js
+++ src/api/apiConstants.js
@@ -1,4 +1,4 @@
-// export const URL = 'https://baloofeathers.herokuapp.com/';
-export const URL = 'http://localhost:3030/';
+export const URL = 'https://baloofeathers.herokuapp.com/';
+// export const URL = 'http://localhost:3030/';
 // export const URL = 'http://192.168.1.108:3030/';
 export const bgColor = '#3CBF95';
